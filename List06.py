5ca16123557a76d7a7ed4dc3afa7d56ae476a509
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-02 11:20:14 +0300

    6

--- List06.py
+++ List06.py
@@ -6,4 +6,12 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    s=0
+    i=0
+    while i<5:
+       if list1[i]==1:
+           s='1' in list1
+           list1[s]=True
+       i+=1
+    return list1
+print(main([1,0,0,0,0]))
