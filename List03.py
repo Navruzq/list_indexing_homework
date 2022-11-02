e9f0c74398010711f4e185a86608fecde9d3dd52
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-02 09:53:50 +0300

    3

--- List03.py
+++ List03.py
@@ -6,5 +6,7 @@ def main(list1,list2):
         list2 (list): parameter
     Returns:
         list: return answer
-    """
-    return
+    """ 
+    
+    return list1+list2
+print(main([1,2,3,4,5,],[6,7,8,9,10]))
