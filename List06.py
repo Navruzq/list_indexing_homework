3751b4a792edc3862c35e6b9de2ea56454eb0b9e
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-02 11:25:02 +0300

    6

--- List06.py
+++ List06.py
@@ -10,8 +10,7 @@ def main(list1):
     i=0
     while i<5:
        if list1[i]==1:
-           s='1' in list1
-           list1[s]=True
+           list1[i]=True
        i+=1
     return list1
-print(main([1,0,0,0,0]))
+print(main([1,1,0,0,0]))
